d4895703b13657374540dd8f00e8eaf98cbe924b
Carllasss <Evilangel3211@gmail.com>
2025-12-17 10:53:32 +0500

    remove rapidfuzz, llm improvements

--- app/utils/ai/engine.py
+++ app/utils/ai/engine.py
@@ -1,6 +1,6 @@
 import logging
 
-from app.utils.ai.filter import filter_products, build_products_context
+from app.utils.ai.filter import build_products_context
 from app.utils.ai.llm import classify_question_ollama, generate_answer_ollama
 from app.config.settings import settings
 from app.utils.ai.sheets import  get_sheet_all_values, get_sheet_all_data
@@ -14,23 +14,15 @@ async def ask_questioin(msg: str) -> str:
     category = classify_question_ollama(msg)
     if category == 'general' or category == 'general.':
         data = get_sheet_all_values(sheet_url, 'Общая информация о компании')
-        logger.debug(data)
     elif category == 'product' or category == 'product.':
         data = get_sheet_all_data(sheet_url, 'Товары')
-        # Попытка уменьшить контекст для иишки
-        # Модель слишком маленкая для обработки полного списка товаров
-        filtered = filter_products(data, msg)
-
-        if not filtered:
-            return "Я не нашёл такой товар в каталоге."
-
-        data = build_products_context(filtered)
-
+        # Передаём все товары в LLM для поиска по всей информации
+        data = build_products_context(data)
+        logger.debug(data)
     else:
         return "Не удалось определить категорию"
 
 
-    logger.debug(data)
     answer = generate_answer_ollama(data, msg)
 
     # Возвращаем только текст ответа, без обёртки в словарь
